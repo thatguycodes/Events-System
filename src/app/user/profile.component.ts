03f8768fd348d96fca75c772bef07633497f705b
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2020-05-05 09:36:42 +0200

    refactor: add multiple validators

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -18,7 +18,7 @@ export class ProfileComponent implements OnInit {
   constructor(private auth: AuthService, private router: Router) {}
   ngOnInit() {
   this.profileForm = new FormGroup({
-    firstName: new FormControl(this.auth.currentUser.firstName, Validators.required),
+    firstName: new FormControl(this.auth.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
     lastName: new FormControl(this.auth.currentUser.lastName, Validators.required)
   });
 }
