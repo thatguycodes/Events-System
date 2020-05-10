5fd41bb2c1cf389c6fc20c1a11fbb276b4259494
L. Theodore Obonye <ltobonye@gmail.com>
2020-05-10 16:18:35 +0200

    feat: filter sessions by level of the session

--- src/app/events/events-details/event-details.component.ts
+++ src/app/events/events-details/event-details.component.ts
@@ -1,35 +1,57 @@
-import { IEvent } from './../shared/event.model';
-import { EventService } from './../shared/events.service';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ISession } from '../shared';
+import { IEvent } from "./../shared/event.model";
+import { EventService } from "./../shared/events.service";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ISession } from "../shared";
 
 @Component({
-  templateUrl: './event-details.component.html',
-  styles: [`
-  .container { padding-left: 10px; padding-right: 20px;}
-  .event-image {height: 100px;}
-  a {cursor: pointer;}
-  `]
+  templateUrl: "./event-details.component.html",
+  styles: [
+    `
+      .container {
+        padding-left: 10px;
+        padding-right: 20px;
+      }
+      .event-image {
+        height: 100px;
+      }
+      a {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class EventDetailsComponent implements OnInit {
   event: IEvent;
   addMode = false;
-  constructor(private eventService: EventService, private activedRoute: ActivatedRoute) {}
+  filterBy = "all";
+
+  constructor(
+    private eventService: EventService,
+    private activedRoute: ActivatedRoute
+  ) {}
   ngOnInit() {
-    this.event = this.eventService.getEvent(+this.activedRoute.snapshot.params.id);
+    this.event = this.eventService.getEvent(
+      +this.activedRoute.snapshot.params.id
+    );
   }
   addSession() {
     this.addMode = true;
   }
   saveNewSession(session: ISession) {
-const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id));
-session.id = nextId + 1;
-this.event.sessions.push(session);
-this.eventService.updateEvent(this.event);
-this.addMode = false;
+    const nextId = Math.max.apply(
+      null,
+      this.event.sessions.map((s) => s.id)
+    );
+    session.id = nextId + 1;
+    this.event.sessions.push(session);
+    this.eventService.updateEvent(this.event);
+    this.addMode = false;
   }
   cancelAddSession() {
     this.addMode = false;
+  }
+  assignFilter(value: string) {
+    this.filterBy = value;
   }
 }
--- src/app/events/events-details/session-list.component.ts
+++ src/app/events/events-details/session-list.component.ts
@@ -1,10 +1,25 @@
-import { ISession } from './../shared/event.model';
-import { Component, Input } from '@angular/core';
+import { ISession } from "./../shared/event.model";
+import { Component, Input, OnChanges } from "@angular/core";
 
 @Component({
-  selector: 'app-session-list',
-  templateUrl: './session-list.component.html'
+  selector: "app-session-list",
+  templateUrl: "./session-list.component.html",
 })
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[];
+  @Input() filterBy: string;
+  visibleSessions: ISession[];
+
+  ngOnChanges(): void {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+    }
+  }
+  filterSessions(filter: string): void {
+    if (filter === 'all') {
+      this.visibleSessions = [...this.sessions];
+    } else {
+      this.visibleSessions = this.sessions.filter(session => session.level.toLowerCase() === filter);
+    }
+  }
 }
