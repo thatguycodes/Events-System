30170ffbd04662a7cf2cbe7f3adfefba6fcfbefd
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2020-05-19 16:45:44 +0200

    style: change the styling of the hearts when you vote

--- src/app/events/events-details/session-list.component.ts
+++ src/app/events/events-details/session-list.component.ts
@@ -12,7 +12,7 @@ export class SessionListComponent implements OnChanges {
   @Input() filterBy: string;
   @Input() sortBy: string;
   visibleSessions: ISession[];
-  constructor(private auth: AuthService, private voterService: VoterService) {}
+  constructor(public auth: AuthService, private voterService: VoterService) {}
 
   ngOnChanges(): void {
     if (this.sessions) {
